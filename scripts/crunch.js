a184768fed53a2a1722870b2c8f4b1b1fee540a5
Madushan Nishantha <j.l.madushan@gmail.com>
2016-08-19 13:08:12 +0530

    new user counter

---
+++ scripts/crunch.js
@@ -0,0 +1,70 @@
+var date = new Date();
+var endTime = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
+var startTime = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() - 1);
+
+var results = db.pings.aggregate([
+  {
+    $match: {
+      "trackedAt": {
+        $gte: startTime,
+        $lte: endTime
+      }
+    }
+  },
+  {
+    $group: {
+      _id:{
+        "userId": "$userId",
+        "year": { $year: "$trackedAt"},
+        "month": { $month: "$trackedAt"},
+        "day": { $dayOfMonth: "$trackedAt"}
+      },
+      //"trackedAt": {$max: "$trackedAt"}
+    }
+  },
+  // {
+  //   $group: {
+  //     _id: {
+  //       "year": "$_id.year",
+  //       "month": "$_id.month",
+  //       "day": "$_id.day"
+  //     },
+  //     count: {$sum: 1}
+  //   }
+  // }
+]).toArray();
+
+var resultsGroupedByDay = _(results).groupBy(function(doc) {
+  return new Date(doc._id.year, doc._id.month, doc._id.day);
+});
+
+_(_(resultsGroupedByDay).mapObject(function(val) {
+  return val.length;
+})).mapObject(function(val, key) {
+  db.statistics.update(
+    {
+      _id: {
+        date: new Date(key),
+        type: "count"
+      }
+    },
+    {
+      $set: {count: val}
+    },
+    {
+      upsert: true
+    }
+  );
+});
+
+// .forEach(function(doc) {
+//   db.statistics.update({
+//     year: doc._id.year,
+//     month: doc._id.month,
+//     day: doc._id.day
+//   }, {
+//     $set: {count: doc.count}
+//   }, {
+//     upsert: true
+//   });
+// });
