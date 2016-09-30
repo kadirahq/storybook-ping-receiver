ba1c7f76db123deba8ebf970738570d48b2c6945
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-09-30 15:36:19 +0530

    Add a way to select the date range.

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
-import {withKnobs, date} from '@kadira/storybook-addon-knobs';
+import { withKnobs, date, select } from '@kadira/storybook-addon-knobs';
 import Chart from './Chart';
 import './styles.css';
 
@@ -10,21 +10,56 @@ stories.addDecorator((story) => (
   <div className='stat-chart'>{story()}</div>
 ));
 
-let to = new Date();
-let from = new Date(new Date() - 1000 * 60 * 60 * 24 * 7);
+let toDate = new Date();
+let fromDate = new Date(new Date() - 1000 * 60 * 60 * 24 * 7);
+
+function getDateRange() {
+  const range = select('Range', {
+    'week': 'Last 7 days',
+    'month': 'Last Month',
+    'custom': 'Custom',
+  }, 'week');
+
+  let from, to;
+
+  switch (range) {
+    case 'week':
+      to = toDate;
+      from = new Date(new Date() - 1000 * 60 * 60 * 24 * 7)
+      break;
+    case 'month':
+      to = toDate;
+      from = new Date(new Date() - 1000 * 60 * 60 * 24 * 30)
+      break;
+    default:
+      from = date('From', fromDate);
+      to = date('To', toDate);
+  }
+
+  return {from, to};
+}
 
 stories.add('Total Users', () => {
-  return <Chart type='totalUsers' from={date('From', from)} to={date('To', to)}/>;
-  })
-  .add('New Users', () => {
-    return <Chart type='newUsers' from={date('From', from)} to={date('To', to)}/>;
-  })
-  .add('Churned Users', () => {
-    return <Chart type='churnedUsers' from={date('From', from)} to={date('To', to)}/>;
-  })
-  .add('Returned Users', () => {
-    return <Chart type='returnedUsers' from={date('From', from)} to={date('To', to)}/>;
-  })
-  .add('All', () => {
-    return <Chart type='all' from={date('From', from)} to={date('To', to)}/>;
-  });
+  const range = getDateRange();
+  return <Chart type='totalUsers' from={range.from} to={range.to}/>;
+});
+
+stories.add('New Users', () => {
+  const range = getDateRange();
+  return <Chart type='newUsers' from={range.from} to={range.to}/>;
+});
+
+stories.add('Churned Users', () => {
+  const range = getDateRange();
+  return <Chart type='churnedUsers' from={range.from} to={range.to}/>;
+});
+
+stories.add('Returned Users', () => {
+  const range = getDateRange();
+  return <Chart type='returnedUsers' from={range.from} to={range.to}/>;
+});
+
+stories.add('All', () => {
+  const range = getDateRange();
+  return <Chart type='all' from={range.from} to={range.to}/>;
+});
